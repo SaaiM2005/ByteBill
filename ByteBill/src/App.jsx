623e82655ec3928c444bee98356335b900610f17
Soumitra Pethe <pethesoumitra2@gmail.com>
2025-04-16 17:00:34 +0530

    soumitra

--- ByteBill/src/App.jsx
+++ ByteBill/src/App.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <div className="h-screen w-full bg-black text-white flex items-center justify-center">hii</div>
+      <div className="h-screen w-full bg-black text-white flex items-center justify-center">
+        Mehfooz gandu
+      </div>
     </>
   )
 }
